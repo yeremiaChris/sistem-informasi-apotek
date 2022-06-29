45d1c97efe07f5207bab288548e1cb9da4f305c3
yeremiaChris <yeremia997@gmail.com>
2022-06-29 21:19:19 +0700

    create ui login system

---
+++ layouts/login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Nuxt id="content" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/auth/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fixed flex justify-center items-center inset-0">
+    <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <AuthHeader />
+
+      <AuthFormSignIn />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "login",
+  name: "Login",
+};
+</script>

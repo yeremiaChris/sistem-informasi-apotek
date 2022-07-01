f2032d764cd458b43989c92c686d20f952d531eb
yeremiaChris <yeremia997@gmail.com>
2022-07-01 17:50:49 +0700

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="font-bold text-2xl">Dashboard</h1>
     <p class="text-sm mt-2 text-gray-400">Sistem Informasi Apotek Pontjol</p>
-    <div class="mt-10 grid grid-cols-4 gap-4">
+    <div class="mt-10 grid md:grid-cols-4 gap-4">
       <HomeCards
         v-for="item in boxes"
         :key="item.title"
@@ -11,19 +11,29 @@
         @leave="leave(item.title)"
       />
     </div>
-    <div class="grid grid-cols-2 mt-10">
+    <div class="grid md:grid-cols-2 mt-10 gap-4">
       <div>
         <h1 class="text-lg font-bold">Grafik Penjualan</h1>
         <div class="flex mt-4 gap-3">
-          <p class="px-2 py-2 border-b">Hari ini</p>
-          <p class="px-2 py-2 border-b">Minggu ini</p>
-          <p class="px-2 py-2 border-b">Bulan ini</p>
+          <p class="px-3 py-2 border-b">Hari ini</p>
+          <p class="px-3 py-2 border-b">Minggu ini</p>
+          <p class="px-3 py-2 border-b">Bulan ini</p>
         </div>
         <client-only>
           <BarChart :height="200" :data="chartData" class="mt-10" />
         </client-only>
       </div>
-      <div>kotak</div>
+      <div>
+        <h1 class="text-lg font-bold">Penjualan Terbaru</h1>
+        <FormsInput
+          class="mb-4 mt-2"
+          label="Search"
+          name="search"
+          v-model="search"
+          placeholder="Search here..."
+        />
+        <ObatTable :headers="headers" :data="data" class="" />
+      </div>
     </div>
   </div>
 </template>
@@ -39,6 +49,9 @@ export default {
         { title: "Pembelian", isHover: false },
         { title: "Penjualan", isHover: false },
       ],
+      headers: ["Nama", "Harga", " Persediaan"],
+      data: [],
+      search: "",
     };
   },
   computed: {
